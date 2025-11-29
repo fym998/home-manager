e3d4a96558cc5c0cc3f5f43c607837e386619bbf
fym998 <61316972+fym998@users.noreply.github.com>
2025-11-29 13:04:06 +0800

    `services.swayidle.package`: use default

--- config/gui/swayidle.nix
+++ config/gui/swayidle.nix
@@ -1,8 +1,6 @@
-{ pkgs, config, ... }:
 {
   services.swayidle = {
     enable = true;
-    package = config.lib.genericLinux.wrapIfEnabled pkgs.swayidle "swayidle";
     timeouts = [
       {
         timeout = 900;
