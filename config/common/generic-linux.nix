c0d5305011b79a173c92db6f5404e39526c6ba46
fym998 <61316972+fym998@users.noreply.github.com>
2025-12-02 00:33:07 +0800

    improve wrapIfEnabled args handling

--- config/common/generic-linux.nix
+++ config/common/generic-linux.nix
@@ -13,7 +13,7 @@
     else
       pkgs.writeShellScriptBin "${builtins.head (lib.splitString " " cmd)}-nix-wrapper" ''
         ${config.lib.shell.export "PATH" (config.lib.shell.prependToVar ":" "PATH" [ "/usr/bin" ])}
-        exec ${cmd} $@
+        exec ${cmd} "$@"
       '';
   lib.genericLinux.getCmd =
     pkg: cmd: if !config.targets.genericLinux.enable then "${pkg}/bin/${cmd}" else "/usr/bin/${cmd}";
