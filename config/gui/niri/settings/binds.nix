5bf761e70c212522f5d6d39a63c885a059be8b92
fym998 <61316972+fym998@users.noreply.github.com>
2025-12-20 23:39:30 +0800

    `programs.niri.settings.binds`: use mkDefault to mark default

--- config/gui/niri/settings/binds.nix
+++ config/gui/niri/settings/binds.nix
@@ -1,215 +1,220 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
-  programs.niri.settings.binds = with config.lib.niri.actions; {
-    # Hotkey overlay
-    "Mod+Shift+Slash".action = show-hotkey-overlay;
+  programs.niri.settings.binds =
+    with config.lib.niri.actions;
+    let
+      inherit (lib) mkDefault;
+    in
+    {
+      # Hotkey overlay
+      "Mod+Shift+Slash".action = mkDefault show-hotkey-overlay;
 
-    # Program launchers
-    "Mod+T" = {
-      action = spawn "alacritty";
-      hotkey-overlay.title = "Open a Terminal: alacritty";
+      # Program launchers
+      "Mod+T" = mkDefault {
+        action = spawn "alacritty";
+        hotkey-overlay.title = "Open a Terminal: alacritty";
+      };
+      "Mod+Space" = mkDefault {
+        action = spawn "fuzzel";
+        hotkey-overlay.title = "Run an Application: fuzzel";
+      };
+
+      # Screen reader toggle
+      "Super+Alt+S" = mkDefault {
+        action = spawn-sh "pkill orca || exec orca";
+        allow-when-locked = true;
+        hotkey-overlay.hidden = true;
+      };
+
+      # Overview toggle
+      "Mod+Tab" = mkDefault {
+        action = toggle-overview;
+        repeat = false;
+      };
+
+      # Window close
+      "Alt+F4" = mkDefault {
+        action = close-window;
+        repeat = false;
+      };
+      "Mod+Q" = mkDefault {
+        action = close-window;
+        repeat = false;
+      };
+
+      # Focus navigation
+      "Mod+Left".action = mkDefault focus-column-left;
+      "Mod+Down".action = mkDefault focus-window-down;
+      "Mod+Up".action = mkDefault focus-window-up;
+      "Mod+Right".action = mkDefault focus-column-right;
+      # "Alt+Tab".action = mkDefault focus-column-right;
+      "Alt+Shift+Tab".action = mkDefault focus-column-left;
+
+      # Move window/column
+      "Mod+Ctrl+Left".action = mkDefault move-column-left;
+      "Mod+Ctrl+Down".action = mkDefault move-window-down;
+      "Mod+Ctrl+Up".action = mkDefault move-window-up;
+      "Mod+Ctrl+Right".action = mkDefault move-column-right;
+
+      # First/Last column
+      "Mod+Home".action = mkDefault focus-column-first;
+      "Mod+End".action = mkDefault focus-column-last;
+      "Mod+Ctrl+Home".action = mkDefault move-column-to-first;
+      "Mod+Ctrl+End".action = mkDefault move-column-to-last;
+
+      # Monitor navigation
+      "Mod+Shift+Left".action = mkDefault focus-monitor-left;
+      "Mod+Shift+Down".action = mkDefault focus-monitor-down;
+      "Mod+Shift+Up".action = mkDefault focus-monitor-up;
+      "Mod+Shift+Right".action = mkDefault focus-monitor-right;
+
+      # Move column to monitor
+      "Mod+Shift+Ctrl+Left".action = mkDefault move-column-to-monitor-left;
+      "Mod+Shift+Ctrl+Down".action = mkDefault move-column-to-monitor-down;
+      "Mod+Shift+Ctrl+Up".action = mkDefault move-column-to-monitor-up;
+      "Mod+Shift+Ctrl+Right".action = mkDefault move-column-to-monitor-right;
+
+      # Workspace navigation
+      "Mod+Page_Down".action = mkDefault focus-workspace-down;
+      "Mod+Page_Up".action = mkDefault focus-workspace-up;
+      "Mod+U".action = mkDefault focus-workspace-down;
+      "Mod+I".action = mkDefault focus-workspace-up;
+      "Mod+Ctrl+Page_Down".action = mkDefault move-column-to-workspace-down;
+      "Mod+Ctrl+Page_Up".action = mkDefault move-column-to-workspace-up;
+      "Mod+Ctrl+U".action = mkDefault move-column-to-workspace-down;
+      "Mod+Ctrl+I".action = mkDefault move-column-to-workspace-up;
+
+      # Move whole workspace
+      "Mod+Shift+Page_Down".action = mkDefault move-workspace-down;
+      "Mod+Shift+Page_Up".action = mkDefault move-workspace-up;
+      "Mod+Shift+U".action = mkDefault move-workspace-down;
+      "Mod+Shift+I".action = mkDefault move-workspace-up;
+
+      # Mouse wheel workspace navigation
+      "Mod+WheelScrollDown" = mkDefault {
+        action = focus-workspace-down;
+        cooldown-ms = 150;
+      };
+      "Mod+WheelScrollUp" = mkDefault {
+        action = focus-workspace-up;
+        cooldown-ms = 150;
+      };
+      "Mod+Ctrl+WheelScrollDown" = mkDefault {
+        action = move-column-to-workspace-down;
+        cooldown-ms = 150;
+      };
+      "Mod+Ctrl+WheelScrollUp" = mkDefault {
+        action = move-column-to-workspace-up;
+        cooldown-ms = 150;
+      };
+
+      # Mouse wheel column navigation
+      "Mod+WheelScrollRight".action = mkDefault focus-column-right;
+      "Mod+WheelScrollLeft".action = mkDefault focus-column-left;
+      "Mod+Ctrl+WheelScrollRight".action = mkDefault move-column-right;
+      "Mod+Ctrl+WheelScrollLeft".action = mkDefault move-column-left;
+
+      # Shifted horizontal scroll
+      "Mod+Shift+WheelScrollDown".action = mkDefault focus-column-right;
+      "Mod+Shift+WheelScrollUp".action = mkDefault focus-column-left;
+      "Mod+Ctrl+Shift+WheelScrollDown".action = mkDefault move-column-right;
+      "Mod+Ctrl+Shift+WheelScrollUp".action = mkDefault move-column-left;
+
+      # Workspace index navigation
+      "Mod+1".action = mkDefault (focus-workspace 1);
+      "Mod+2".action = mkDefault (focus-workspace 2);
+      "Mod+3".action = mkDefault (focus-workspace 3);
+      "Mod+4".action = mkDefault (focus-workspace 4);
+      "Mod+5".action = mkDefault (focus-workspace 5);
+      "Mod+6".action = mkDefault (focus-workspace 6);
+      "Mod+7".action = mkDefault (focus-workspace 7);
+      "Mod+8".action = mkDefault (focus-workspace 8);
+      "Mod+9".action = mkDefault (focus-workspace 9);
+      # "Mod+Ctrl+1".action = mkDefault (move-column-to-workspace 1);
+      # "Mod+Ctrl+2".action = mkDefault (move-column-to-workspace 2);
+      # "Mod+Ctrl+3".action = mkDefault (move-column-to-workspace 3);
+      # "Mod+Ctrl+4".action = mkDefault (move-column-to-workspace 4);
+      # "Mod+Ctrl+5".action = mkDefault (move-column-to-workspace 5);
+      # "Mod+Ctrl+6".action = mkDefault (move-column-to-workspace 6);
+      # "Mod+Ctrl+7".action = mkDefault (move-column-to-workspace 7);
+      # "Mod+Ctrl+8".action = mkDefault (move-column-to-workspace 8);
+      # "Mod+Ctrl+9".action = mkDefault (move-column-to-workspace 9);
+
+      # Consume/expel window
+      "Mod+BracketLeft".action = mkDefault consume-or-expel-window-left;
+      "Mod+BracketRight".action = mkDefault consume-or-expel-window-right;
+
+      # Column/window sizing presets
+      "Mod+R".action = mkDefault switch-preset-column-width;
+      "Mod+Shift+R".action = mkDefault switch-preset-window-height;
+      "Mod+Ctrl+R".action = mkDefault reset-window-height;
+      "Mod+F".action = mkDefault maximize-column;
+      "Mod+Shift+F".action = mkDefault fullscreen-window;
+      "Mod+Ctrl+F".action = mkDefault expand-column-to-available-width;
+
+      # Column width adjustment
+      "Mod+Minus".action = mkDefault (set-column-width "-10%");
+      "Mod+Equal".action = mkDefault (set-column-width "+10%");
+
+      # Window height adjustment
+      "Mod+Shift+Minus".action = mkDefault (set-window-height "-10%");
+      "Mod+Shift+Equal".action = mkDefault (set-window-height "+10%");
+
+      # Tabbed display toggle
+      "Mod+W".action = mkDefault toggle-column-tabbed-display;
+
+      # Screenshots
+      "Print".action = mkDefault { screenshot = [ ]; };
+      "Ctrl+Print".action = mkDefault { screenshot-window = [ ]; };
+      "Alt+Print".action = mkDefault { screenshot-screen = [ ]; };
+      ## Laptop
+      "Super+Shift+S".action = mkDefault { screenshot = [ ]; };
+      "Ctrl+Super+Shift+S".action = mkDefault { screenshot-window = [ ]; };
+      "Alt+Super+Shift+S".action = mkDefault { screenshot-screen = [ ]; };
+
+      # Inhibitor escape hatch
+      "Mod+Escape" = mkDefault {
+        action = toggle-keyboard-shortcuts-inhibit;
+        allow-inhibiting = false;
+      };
+
+      # Quit
+      "Mod+Shift+E".action = mkDefault quit;
+      "Ctrl+Alt+Delete".action = mkDefault quit;
+
+      # Power off monitors
+      "Mod+Shift+P".action = mkDefault power-off-monitors;
+
+      "Super+L" = mkDefault {
+        action = spawn "swaylock";
+        hotkey-overlay.title = "Lock the Screen: swaylock";
+      };
+
+      "XF86AudioRaiseVolume" = mkDefault {
+        action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+";
+        allow-when-locked = true;
+      };
+      "XF86AudioLowerVolume" = mkDefault {
+        action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
+        allow-when-locked = true;
+      };
+      "XF86AudioMute" = mkDefault {
+        action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+        allow-when-locked = true;
+      };
+      "XF86AudioMicMute" = mkDefault {
+        action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+        allow-when-locked = true;
+      };
+
+      "XF86MonBrightnessUp" = mkDefault {
+        action = spawn "brightnessctl" "--class=backlight" "set" "5%+" "-d" "intel_backlight";
+        allow-when-locked = true;
+      };
+      "XF86MonBrightnessDown" = mkDefault {
+        action = spawn "brightnessctl" "--class=backlight" "set" "5%-" "-d" "intel_backlight";
+        allow-when-locked = true;
+      };
     };
-    "Mod+Space" = {
-      action = spawn "fuzzel";
-      hotkey-overlay.title = "Run an Application: fuzzel";
-    };
-
-    # Screen reader toggle
-    "Super+Alt+S" = {
-      action = spawn-sh "pkill orca || exec orca";
-      allow-when-locked = true;
-      hotkey-overlay.hidden = true;
-    };
-
-    # Overview toggle
-    "Mod+Tab" = {
-      action = toggle-overview;
-      repeat = false;
-    };
-
-    # Window close
-    "Alt+F4" = {
-      action = close-window;
-      repeat = false;
-    };
-    "Mod+Q" = {
-      action = close-window;
-      repeat = false;
-    };
-
-    # Focus navigation
-    "Mod+Left".action = focus-column-left;
-    "Mod+Down".action = focus-window-down;
-    "Mod+Up".action = focus-window-up;
-    "Mod+Right".action = focus-column-right;
-    "Alt+Tab".action = focus-column-right;
-    "Alt+Shift+Tab".action = focus-column-left;
-
-    # Move window/column
-    "Mod+Ctrl+Left".action = move-column-left;
-    "Mod+Ctrl+Down".action = move-window-down;
-    "Mod+Ctrl+Up".action = move-window-up;
-    "Mod+Ctrl+Right".action = move-column-right;
-
-    # First/Last column
-    "Mod+Home".action = focus-column-first;
-    "Mod+End".action = focus-column-last;
-    "Mod+Ctrl+Home".action = move-column-to-first;
-    "Mod+Ctrl+End".action = move-column-to-last;
-
-    # Monitor navigation
-    "Mod+Shift+Left".action = focus-monitor-left;
-    "Mod+Shift+Down".action = focus-monitor-down;
-    "Mod+Shift+Up".action = focus-monitor-up;
-    "Mod+Shift+Right".action = focus-monitor-right;
-
-    # Move column to monitor
-    "Mod+Shift+Ctrl+Left".action = move-column-to-monitor-left;
-    "Mod+Shift+Ctrl+Down".action = move-column-to-monitor-down;
-    "Mod+Shift+Ctrl+Up".action = move-column-to-monitor-up;
-    "Mod+Shift+Ctrl+Right".action = move-column-to-monitor-right;
-
-    # Workspace navigation
-    "Mod+Page_Down".action = focus-workspace-down;
-    "Mod+Page_Up".action = focus-workspace-up;
-    "Mod+U".action = focus-workspace-down;
-    "Mod+I".action = focus-workspace-up;
-    "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
-    "Mod+Ctrl+Page_Up".action = move-column-to-workspace-up;
-    "Mod+Ctrl+U".action = move-column-to-workspace-down;
-    "Mod+Ctrl+I".action = move-column-to-workspace-up;
-
-    # Move whole workspace
-    "Mod+Shift+Page_Down".action = move-workspace-down;
-    "Mod+Shift+Page_Up".action = move-workspace-up;
-    "Mod+Shift+U".action = move-workspace-down;
-    "Mod+Shift+I".action = move-workspace-up;
-
-    # Mouse wheel workspace navigation
-    "Mod+WheelScrollDown" = {
-      action = focus-workspace-down;
-      cooldown-ms = 150;
-    };
-    "Mod+WheelScrollUp" = {
-      action = focus-workspace-up;
-      cooldown-ms = 150;
-    };
-    "Mod+Ctrl+WheelScrollDown" = {
-      action = move-column-to-workspace-down;
-      cooldown-ms = 150;
-    };
-    "Mod+Ctrl+WheelScrollUp" = {
-      action = move-column-to-workspace-up;
-      cooldown-ms = 150;
-    };
-
-    # Mouse wheel column navigation
-    "Mod+WheelScrollRight".action = focus-column-right;
-    "Mod+WheelScrollLeft".action = focus-column-left;
-    "Mod+Ctrl+WheelScrollRight".action = move-column-right;
-    "Mod+Ctrl+WheelScrollLeft".action = move-column-left;
-
-    # Shifted horizontal scroll
-    "Mod+Shift+WheelScrollDown".action = focus-column-right;
-    "Mod+Shift+WheelScrollUp".action = focus-column-left;
-    "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
-    "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
-
-    # Workspace index navigation
-    "Mod+1".action = focus-workspace 1;
-    "Mod+2".action = focus-workspace 2;
-    "Mod+3".action = focus-workspace 3;
-    "Mod+4".action = focus-workspace 4;
-    "Mod+5".action = focus-workspace 5;
-    "Mod+6".action = focus-workspace 6;
-    "Mod+7".action = focus-workspace 7;
-    "Mod+8".action = focus-workspace 8;
-    "Mod+9".action = focus-workspace 9;
-    # "Mod+Ctrl+1".action = move-column-to-workspace 1;
-    # "Mod+Ctrl+2".action = move-column-to-workspace 2;
-    # "Mod+Ctrl+3".action = move-column-to-workspace 3;
-    # "Mod+Ctrl+4".action = move-column-to-workspace 4;
-    # "Mod+Ctrl+5".action = move-column-to-workspace 5;
-    # "Mod+Ctrl+6".action = move-column-to-workspace 6;
-    # "Mod+Ctrl+7".action = move-column-to-workspace 7;
-    # "Mod+Ctrl+8".action = move-column-to-workspace 8;
-    # "Mod+Ctrl+9".action = move-column-to-workspace 9;
-
-    # Consume/expel window
-    "Mod+BracketLeft".action = consume-or-expel-window-left;
-    "Mod+BracketRight".action = consume-or-expel-window-right;
-
-    # Column/window sizing presets
-    "Mod+R".action = switch-preset-column-width;
-    "Mod+Shift+R".action = switch-preset-window-height;
-    "Mod+Ctrl+R".action = reset-window-height;
-    "Mod+F".action = maximize-column;
-    "Mod+Shift+F".action = fullscreen-window;
-    "Mod+Ctrl+F".action = expand-column-to-available-width;
-
-    # Column width adjustment
-    "Mod+Minus".action = set-column-width "-10%";
-    "Mod+Equal".action = set-column-width "+10%";
-
-    # Window height adjustment
-    "Mod+Shift+Minus".action = set-window-height "-10%";
-    "Mod+Shift+Equal".action = set-window-height "+10%";
-
-    # Tabbed display toggle
-    "Mod+W".action = toggle-column-tabbed-display;
-
-    # Screenshots
-    "Print".action.screenshot = [ ];
-    "Ctrl+Print".action.screenshot-window = [ ];
-    "Alt+Print".action.screenshot-screen = [ ];
-    ## Laptop
-    "Super+Shift+S".action.screenshot = [ ];
-    "Ctrl+Super+Shift+S".action.screenshot-window = [ ];
-    "Alt+Super+Shift+S".action.screenshot-screen = [ ];
-
-    # Inhibitor escape hatch
-    "Mod+Escape" = {
-      action = toggle-keyboard-shortcuts-inhibit;
-      allow-inhibiting = false;
-    };
-
-    # Quit
-    "Mod+Shift+E".action = quit;
-    "Ctrl+Alt+Delete".action = quit;
-
-    # Power off monitors
-    "Mod+Shift+P".action = power-off-monitors;
-
-    "Super+L" = {
-      action = spawn "swaylock";
-      hotkey-overlay.title = "Lock the Screen: swaylock";
-    };
-
-    "XF86AudioRaiseVolume" = {
-      action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+";
-      allow-when-locked = true;
-    };
-    "XF86AudioLowerVolume" = {
-      action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
-      allow-when-locked = true;
-    };
-    "XF86AudioMute" = {
-      action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-      allow-when-locked = true;
-    };
-    "XF86AudioMicMute" = {
-      action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
-      allow-when-locked = true;
-    };
-
-    "XF86MonBrightnessUp" = {
-      action = spawn "brightnessctl" "--class=backlight" "set" "5%+" "-d" "intel_backlight";
-      allow-when-locked = true;
-    };
-    "XF86MonBrightnessDown" = {
-      action = spawn "brightnessctl" "--class=backlight" "set" "5%-" "-d" "intel_backlight";
-      allow-when-locked = true;
-    };
-  };
 }
--- config/gui/noctalia-shell/niri.nix
+++ config/gui/noctalia-shell/niri.nix
@@ -2,83 +2,78 @@
 
 {
   programs.niri.settings = lib.mkIf config.programs.noctalia-shell.enable {
-    binds =
-      with config.lib.niri.actions;
-      let
-        inherit (lib) mkForce;
-      in
-      {
-        # Core Noctalia
-        "Mod+S" = {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "controlCenter" "toggle";
-          hotkey-overlay.title = "Noctalia ControlCenter";
-        };
-        "Mod+Comma" = {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "settings" "toggle";
-          hotkey-overlay.title = "Noctalia Settings";
-        };
-
-        # Audio
-        "XF86AudioRaiseVolume" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "increase";
-          allow-when-locked = true;
-        };
-        "XF86AudioLowerVolume" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "decrease";
-          allow-when-locked = true;
-        };
-        "XF86AudioMute" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "muteOutput";
-          allow-when-locked = true;
-        };
-        "XF86AudioMicMute" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "muteInput";
-          allow-when-locked = true;
-        };
-
-        # Media
-        "XF86AudioPlay" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "playPause";
-          allow-when-locked = true;
-        };
-        "XF86AudioStop" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "pause";
-          allow-when-locked = true;
-        };
-        "XF86AudioNext" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "next";
-          allow-when-locked = true;
-        };
-        "XF86AudioPrev" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "previous";
-          allow-when-locked = true;
-        };
-
-        # # Brightness
-        # "XF86MonBrightnessUp" = mkForce {
-        #   action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "brightness" "increase";
-        #   allow-when-locked = true;
-        # };
-        # "XF86MonBrightnessDown" = mkForce {
-        #   action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "brightness" "decrease";
-        #   allow-when-locked = true;
-        # };
-
-        # Utilities
-        "Mod+V" = {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "clipboard";
-          hotkey-overlay.title = "Noctalia Clipboard History";
-        };
-        "XF86Calculator".action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "calculator";
-        "Super+L" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "lockScreen" "lock";
-          hotkey-overlay.title = "Lock the Screen";
-        };
-        "Mod+Space" = mkForce {
-          action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "toggle";
-          hotkey-overlay.title = "Noctalia Launcher";
-        };
+    binds = with config.lib.niri.actions; {
+      # Core Noctalia
+      "Mod+S" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "controlCenter" "toggle";
+        hotkey-overlay.title = "Noctalia ControlCenter";
       };
+      "Mod+Comma" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "settings" "toggle";
+        hotkey-overlay.title = "Noctalia Settings";
+      };
+
+      # Audio
+      "XF86AudioRaiseVolume" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "increase";
+        allow-when-locked = true;
+      };
+      "XF86AudioLowerVolume" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "decrease";
+        allow-when-locked = true;
+      };
+      "XF86AudioMute" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "muteOutput";
+        allow-when-locked = true;
+      };
+      "XF86AudioMicMute" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "volume" "muteInput";
+        allow-when-locked = true;
+      };
+
+      # Media
+      "XF86AudioPlay" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "playPause";
+        allow-when-locked = true;
+      };
+      "XF86AudioStop" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "pause";
+        allow-when-locked = true;
+      };
+      "XF86AudioNext" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "next";
+        allow-when-locked = true;
+      };
+      "XF86AudioPrev" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "media" "previous";
+        allow-when-locked = true;
+      };
+
+      # # Brightness
+      # "XF86MonBrightnessUp" =  {
+      #   action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "brightness" "increase";
+      #   allow-when-locked = true;
+      # };
+      # "XF86MonBrightnessDown" =  {
+      #   action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "brightness" "decrease";
+      #   allow-when-locked = true;
+      # };
+
+      # Utilities
+      "Mod+V" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "clipboard";
+        hotkey-overlay.title = "Noctalia Clipboard History";
+      };
+      "XF86Calculator".action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "calculator";
+      "Super+L" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "lockScreen" "lock";
+        hotkey-overlay.title = "Lock the Screen";
+      };
+      "Mod+Space" = {
+        action = spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "toggle";
+        hotkey-overlay.title = "Noctalia Launcher";
+      };
+    };
 
     window-rules = [
       # Example: enable rounded corners for all windows
