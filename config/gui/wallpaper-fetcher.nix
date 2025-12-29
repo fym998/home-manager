2daf4b9b560498aa9d2eb6299d4ae1f9aeabb9d9
fym998 <61316972+fym998@users.noreply.github.com>
2025-12-29 17:21:14 +0800

    wallpaper-fetcher: use systemd timer

--- config/gui/wallpaper-fetcher.nix
+++ config/gui/wallpaper-fetcher.nix
@@ -7,10 +7,16 @@
 {
   systemd.user.services.wallpaper-fetcher = {
     Service = {
-      ExecStart = "${lib.getExe pkgsFrom.fym998-nur.wallpaper-fetcher} -d -i 21600 --debug";
+      ExecStart = "${lib.getExe pkgsFrom.fym998-nur.wallpaper-fetcher} -d --debug";
     };
+  };
+  systemd.user.timers.wallpaper-fetcher = {
     Install = {
       WantedBy = [ config.wayland.systemd.target ];
+    };
+    Timer = {
+      OnActiveSec = "1min";
+      OnUnitActiveSec = "1day";
     };
   };
 }
